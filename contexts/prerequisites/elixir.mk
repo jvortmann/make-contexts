20a94b3b204f1bd2ca007b3b66654a9c06e13767
João Vortmann <jvortmann@gmail.com>
2020-03-12 19:35:16 -0300

    feature: Change elixir dependency to setup when missing only

--- contexts/prerequisites/elixir.mk
+++ contexts/prerequisites/elixir.mk
@@ -1,6 +1,6 @@
 .PHONY: mix
 
 mix:
-	@cd $(base_path) && mix local.hex --force
-	@cd $(base_path) && mix local.rebar --force
+	@cd $(base_path) && mix local.hex --if-missing
+	@cd $(base_path) && mix local.rebar --if-missing
 	@cd $(base_path) && mix deps.get
